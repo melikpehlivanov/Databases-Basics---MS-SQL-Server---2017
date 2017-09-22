83db046837d953dd4ca8ea94f668ad342cc8ce60
Melik <melikpehlivanov@gmail.com>
2017-09-22 09:55:14 +0300

    Update Basic Crud - All Problems.sql

--- Basic Crud/Basic Crud - All Problems.sql
+++ Basic Crud/Basic Crud - All Problems.sql
@@ -56,7 +56,6 @@ FROM Employees
 CREATE VIEW V_EmployeeNameJobTitle AS
 SELECT FirstName + ' ' + ISNULL (MiddleName, '') + ' ' + LastName AS [Full Name], JobTitle AS [Job Title]
 FROM Employees
-SELECT * FROM V_EmployeeNameJobTitle
 
 --18.
 SELECT DISTINCT JobTitle FROM Employees
